9960d3ca503864976d4cf4aafb8b410d1713a3a6
Emeline <emeline.hourmand@gmail.com>
2019-05-07 10:11:53 +0200

    comment ProfileManager

--- src/fr/eni/enienchere/bll/ProfileManager.java
+++ src/fr/eni/enienchere/bll/ProfileManager.java
@@ -1,12 +1,16 @@
 package fr.eni.enienchere.bll;
 
-import fr.eni.enienchere.EnchereException;
 import fr.eni.enienchere.bll.exception.BLLException;
 import fr.eni.enienchere.bo.User;
 import fr.eni.enienchere.dal.dao.DAOFactory;
 import fr.eni.enienchere.dal.dao.UserDAO;
 import fr.eni.enienchere.dal.exception.DALException;
 
+/**
+ *
+ * @author ehourman2019
+ *
+ */
 public class ProfileManager {
 
     private static UserDAO userDAO;
@@ -19,6 +23,12 @@ public class ProfileManager {
         userDAO = DAOFactory.getUserDAO();
     }
 
+    /**
+     * add user in bdd
+     * @param id
+     * @return
+     * @throws BLLException
+     */
    public User selectUserProfile(long id) throws BLLException{
        try  {
            return (User) userDAO.selectById(id);
@@ -28,6 +38,11 @@ public class ProfileManager {
        return null;
     }
 
+    /**
+     * update user in bdd
+     * @param user
+     * @throws BLLException
+     */
     public void updateUserProfile(User user) throws BLLException {
         try {
             userDAO.update(user);
@@ -36,6 +51,11 @@ public class ProfileManager {
         }
     }
 
+    /**
+     * delete user in bdd
+     * @param id
+     * @throws BLLException
+     */
     public void deleteUserProfile(long id) throws BLLException {
         try {
             userDAO.delete(id);
